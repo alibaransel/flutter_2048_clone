62c58f77618e0c1091d1962ddccfb243a65be298
alibaransel <alibaransel25@gmail.com>
2023-05-09 02:01:53 +0300

    Some refactoring

--- lib/ui/swipe_detector.dart
+++ lib/ui/swipe_detector.dart
@@ -18,29 +18,32 @@ class SwipeDetector extends StatefulWidget {
 }
 
 class _SwipeDetectorState extends State<SwipeDetector> {
+  static const double _halfQuarter = 22.5;
+
   Offset? startOffset;
+
+  SwipeDirection? _angleToSwipeDirection(int angle) {
+    if ((angle <= _halfQuarter) || (360 - angle <= _halfQuarter)) return SwipeDirection.right;
+    if ((angle - 90).abs() <= _halfQuarter) return SwipeDirection.up;
+    if ((angle - 180).abs() <= _halfQuarter) return SwipeDirection.left;
+    if ((angle - 270).abs() <= _halfQuarter) return SwipeDirection.down;
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Listener(
-      onPointerDown: (topDownDetails) {
-        startOffset = topDownDetails.localPosition;
+      onPointerDown: (pointerDownDetails) {
+        startOffset = pointerDownDetails.localPosition;
       },
-      onPointerUp: (tapUpDetails) {
+      onPointerUp: (pointerUpDetails) {
         if (startOffset == null) return;
-        final Offset differenceOffset = tapUpDetails.localPosition - startOffset!;
+        final Offset differenceOffset = pointerUpDetails.localPosition - startOffset!;
         if (differenceOffset.distance < 50) return;
         int angle = -differenceOffset.direction * 180 ~/ pi;
         if (angle < 0) angle += 360;
-        if ((angle <= 22) || (360 - angle <= 22)) {
-          widget.onSwipe(SwipeDirection.right);
-        } else if ((angle - 90).abs() <= 22) {
-          widget.onSwipe(SwipeDirection.up);
-        } else if ((angle - 180).abs() <= 22) {
-          widget.onSwipe(SwipeDirection.left);
-        } else if ((angle - 270).abs() <= 22) {
-          widget.onSwipe(SwipeDirection.down);
-        }
+        final SwipeDirection? swipeDirection = _angleToSwipeDirection(angle);
+        if (swipeDirection != null) widget.onSwipe(swipeDirection);
         startOffset = null;
       },
       child: widget.child,

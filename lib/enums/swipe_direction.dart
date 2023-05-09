77471491b583b02596f7d82444b92d27722a1270
alibaransel <alibaransel25@gmail.com>
2023-05-09 05:22:16 +0300

    Axis property added

--- lib/enums/swipe_direction.dart
+++ lib/enums/swipe_direction.dart
@@ -1,6 +1,14 @@
+import 'package:flutter/material.dart';
+
 enum SwipeDirection {
-  up,
-  down,
-  right,
-  left,
+  up(Axis.vertical),
+  down(Axis.vertical),
+  right(Axis.horizontal),
+  left(Axis.horizontal);
+
+  const SwipeDirection(
+    this.axis,
+  );
+
+  final Axis axis;
 }
